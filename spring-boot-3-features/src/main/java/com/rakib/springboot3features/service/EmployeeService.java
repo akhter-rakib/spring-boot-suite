e08b427a2fab48ae266418ae576a3d5f1dd54e4a
rakibccj <rakibccj@gmail.com>
2023-01-03 16:57:24 +0600

    added Observability feature
    -- enable to see all metrics via (host:port/actuator/metrics/)
    -- enable specific metrics visibility via   (host:port/actuator/metrics/endPoint)

--- spring-boot-3-features/src/main/java/com/rakib/springboot3features/service/EmployeeService.java
+++ spring-boot-3-features/src/main/java/com/rakib/springboot3features/service/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.rakib.springboot3features.service;
 
 import com.rakib.springboot3features.dto.Employee;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,21 +11,26 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
 
+    private final ObservationRegistry registry;
     List<Employee> employeeList = new ArrayList<>();
 
     public Employee addEmployee(Employee employee) {
         employeeList.add(employee);
-        return employee;
+        return Observation.createNotStarted("addEmployee", registry).observe(() -> employee);
+
     }
 
     public List<Employee> getEmployeeList() {
-        return employeeList;
+        return Observation.createNotStarted("getEmployeeList", registry).observe(() -> employeeList);
     }
 
     public Employee getEmployee(Integer id) {
-        return employeeList.stream().filter(employee -> Objects.equals(employee.id(), id))
-                .findAny().orElseThrow(() -> new RuntimeException("Customer Not found with id " + id));
+        return Observation.createNotStarted("getEmployee", registry).observe(() ->
+                employeeList.stream().filter(employee -> Objects.equals(employee.id(), id))
+                        .findAny().orElseThrow(() -> new RuntimeException("Customer Not found with id " + id)));
+
     }
 }
