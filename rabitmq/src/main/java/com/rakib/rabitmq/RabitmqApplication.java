7fc7ccd362f9f4d72cafcbe0c1453b7150fb89de
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-05-18 17:33:55 +0600

    Initial project -- dockerize project

---
+++ rabitmq/src/main/java/com/rakib/rabitmq/RabitmqApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.rabitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabitmqApplication.class, args);
+	}
+
+}
---
+++ rabitmq/src/test/java/com/rakib/rabitmq/RabitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.rabitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
