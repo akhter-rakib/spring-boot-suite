68804056217924b9a2a85499c8b9432df66946b9
farzana-yesmin <farzana_cse15@yahoo.com>
2023-05-24 23:38:21 +0600

    project initiate -- dockerize mysql & redis -- mysql & redis properties added in application.yml file

---
+++ spring-data-redis/src/test/java/com/rakib/springdataredis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.springdataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
