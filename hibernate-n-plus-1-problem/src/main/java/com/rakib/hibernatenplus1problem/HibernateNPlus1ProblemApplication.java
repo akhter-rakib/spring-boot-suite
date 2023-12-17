207692b9361970d452ffc05a8ccee61a5474653f
Rakib <rakibccj@gmail.com>
2023-12-17 15:48:12 +0600

    project initiate

---
+++ hibernate-n-plus-1-problem/src/main/java/com/rakib/hibernatenplus1problem/HibernateNPlus1ProblemApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.hibernatenplus1problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateNPlus1ProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateNPlus1ProblemApplication.class, args);
+    }
+
+}
---
+++ hibernate-n-plus-1-problem/src/test/java/com/rakib/hibernatenplus1problem/HibernateNPlus1ProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.hibernatenplus1problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateNPlus1ProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
