e854d0547e38e3f15e069e20362000f3d3bb046e
Rakib <rakibccj@gmail.com>
2024-03-24 12:45:02 +0600

    initiate project

---
+++ solid-principle/src/main/java/com/rakib/solidprinciple/SolidPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.solidprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidPrincipleApplication.class, args);
+	}
+
+}
