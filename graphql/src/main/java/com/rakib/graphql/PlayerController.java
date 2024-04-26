e3a7a295727b0c51693f06970f5d3ee624513029
Rakib <rakibccj@gmail.com>
2024-04-27 00:33:56 +0600

    CRUD at graphql

--- graphql/src/main/java/com/rakib/graphql/PlayerController.java
+++ graphql/src/main/java/com/rakib/graphql/PlayerController.java
@@ -1,11 +1,15 @@
 package com.rakib.graphql;
 
 import com.rakib.graphql.model.Player;
+import com.rakib.graphql.model.Team;
 import com.rakib.graphql.service.PlayerService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PlayerController {
@@ -21,4 +25,25 @@ public class PlayerController {
     public List<Player> findAll() {
         return playerService.findAll();
     }
+
+    @QueryMapping
+    public Optional<Player> findOne(@Argument Integer id) {
+        return playerService.findOne(id);
+    }
+
+    @MutationMapping
+    public Player createPlayer(@Argument String name, @Argument Team team) {
+        return playerService.create(name, team);
+    }
+
+    @MutationMapping
+    public Player updatePlayer(@Argument Integer id, @Argument String name, @Argument Team team) {
+        return playerService.updatePlayer(id, name, team);
+    }
+
+    @MutationMapping
+    public Player deletePlayer(@Argument Integer id) {
+        return playerService.deletePlayer(id);
+    }
+
 }
